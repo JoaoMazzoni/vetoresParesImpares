c17223c5f6d13745ef2e1d13409f2244df18663e
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-22 15:20:17 -0300

    Add project files.

---
+++ vetoresParesImpares/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] numeros = new int[10];
+//int[] pares = new int[10];
+//int[] impares = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Digite o {0}° número: ", i + 1);
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("\nNúmeros digitados: ");
+for (int i = 0; i < 10; i++)
+{
+    Console.Write(numeros[i] + " ");
+}
+
+Console.ReadKey();
+
+Console.WriteLine("\nNúmeros pares: ");
+for (int i = 0; i < 10; i++)
+{ 
+    if (numeros[i] % 2 == 0)
+    {
+        Console.Write(numeros[i] + "");
+    }
+ }
+Console.ReadKey();
+
+Console.WriteLine("\nNúmeros impares: ");
+for (int i = 0; i < 10; i++)
+{
+    if (numeros[i] % 2 != 0)
+    {
+        Console.Write(numeros[i] + "");
+    }
+}
+Console.ReadKey();
+
+Console.WriteLine("\nPrimeiros e ultimos números: ");
+Console.WriteLine(numeros[0]);
+Console.WriteLine(numeros[9]);
